24c704b4ef8cb7ccbb293ee580c7351a12ed9b73
dag7dev <dag7@protonmail.com>
2022-05-10 12:10:44 +0200

    packages definition and first index.ts

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import { createConnection } from 'typeorm';
+import cookieParser from "cookie-parser";
+createConnection().then(connection => {
+    const app = express();
+
+    // json as return
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(cors({
+        credentials: true,      // frontend now can access cookies
+        origin: ["http://localhost:8800"]
+    }));
+    
+    app.listen(8000, () => {
+        console.log("Listening to port 8000")
+    });
+})
