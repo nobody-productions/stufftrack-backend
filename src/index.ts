0c508d7dd055389e0572e374dd630c672f8ebf3d
dag7dev <dag7@protonmail.com>
2022-05-10 14:50:42 +0200

    feat: now routes are working

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 import express, {Request, Response} from 'express';
 import cors from 'cors';
+import { routes } from './routes';
 import { createConnection } from 'typeorm';
 import cookieParser from "cookie-parser";
 createConnection().then(connection => {
@@ -14,6 +15,8 @@ createConnection().then(connection => {
         credentials: true,      // frontend now can access cookies
         origin: ["http://localhost:8800"]
     }));
+    
+    routes(app);
     
     app.listen(8000, () => {
         console.log("Listening to port 8000")
