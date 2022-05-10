297eccd9acff7d621940ee4ca92a6402291b0edd
dag7dev <dag7@protonmail.com>
2022-05-10 15:03:36 +0200

    removed: routes and user oontroller I will focus on it later, I want to focus on models first

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,6 @@
 import express, { Router } from "express";
-import { CreateUser, DeleteUser, GetUser, UpdateUser, Users } from "./controller/user.controller";
 
 export const routes = (router: Router) => {
     // user routes
-    router.get('/api/v1/users', Users)
-    router.get("/api/v1/users/:id", GetUser)
-    router.post('/api/v1/users', CreateUser)
-    router.put("/api/v1/users/:id", UpdateUser)
-    router.delete("/api/v1/users/:id", DeleteUser)
 
 }
