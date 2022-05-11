0dc182d4c87f15d13c5e84a19e005b7a98668fc0
dag7dev <dag7@protonmail.com>
2022-05-11 15:12:44 +0200

    feat: auth middleware - to use certain roots, you need to be logged

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
 import {Login, Register, Logout, Profile} from "./controller/auth.controller";
+import {AuthMiddleware} from "./middleware/auth.middleware";
 
 export const routes = (router: Router) => {
     // user routes
     router.post("/api/v1/register", Register)
     router.post("/api/v1/login", Login)
     router.post("/api/v1/logout", Logout)
-    router.get("/api/v1/profile", Profile)
+    router.get("/api/v1/profile", AuthMiddleware, Profile)
 
 }
