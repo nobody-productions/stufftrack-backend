c7344c9255ed1807a56829a7411e008c3c041fe6
dag7dev <dag7@protonmail.com>
2022-05-11 18:19:46 +0200

    feat: remake seed

---
+++ src/seeder/remake.seed.ts
@@ -0,0 +1,16 @@
+import {createConnections, getManager} from "typeorm";
+import { Videogame } from "../entity/videogames/videogame.entity";
+
+createConnections().then(async () => {
+    // pokemon zaffiro alpha é remake di pokemon zaffiro
+    const repository = getManager().getRepository(Videogame)
+    const vg1 = await repository.findOne({where: {id: 6}})
+    const vg2 = await repository.findOne({where: {id: 7}})
+
+    vg1.videogames = []
+    vg1.videogames.push(vg2)
+
+    await repository.save(vg1)
+
+    process.exit(0);
+})
