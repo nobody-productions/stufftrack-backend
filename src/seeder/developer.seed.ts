ca5e931c5cc21770dee900a2d133aaa2a3488cb0
dag7dev <dag7@protonmail.com>
2022-05-11 15:48:14 +0200

    feat: seeder for developers

---
+++ src/seeder/developer.seed.ts
@@ -0,0 +1,38 @@
+import {createConnections, getManager} from "typeorm";
+import {Developer} from "../entity/videogames/developer.entity";
+
+createConnections().then(async () => {
+    const repository = getManager().getRepository(Developer);
+
+    await repository.save({
+        name: "Nintendo"
+    })
+    await repository.save({
+        name: "SEGA"
+    })
+    await repository.save({
+        name: "Sony"
+    })
+    await repository.save({
+        name: "Microsoft"
+    })
+    await repository.save({
+        name: "TobyFox"
+    })
+    await repository.save({
+        name: "Meat Team"
+    })
+    await repository.save({
+        name: "DONTNOD"
+    })
+    await repository.save({
+        name: "Ubisoft"
+    })
+    await repository.save({
+        name: "Bethesda"
+    })
+    await repository.save({
+        name: "Capcom"
+    })
+    process.exit(0);
+})
