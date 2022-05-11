948c1db1fec1fc88a30b437a4cdfc784b1946ab1
dag7dev <dag7@protonmail.com>
2022-05-11 14:08:30 +0200

    clean: removed unusued stuff

--- src/entity/permission.entity.ts
+++ src/entity/permission.entity.ts
@@ -1,5 +1,4 @@
-import {Column, Entity, ManyToMany, PrimaryGeneratedColumn} from "typeorm";
-import {JoinTable} from "typeorm";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
 export class Permission {
