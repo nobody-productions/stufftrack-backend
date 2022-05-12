d32333814659bd61da1550d7d135cb6ec4b4ca72
dag7dev <dag7@protonmail.com>
2022-05-12 13:34:07 +0200

    refactor: user_videogame now as separated table - it does include additional fields too!

--- src/entity/videogame/videogame.entity.ts
+++ src/entity/videogame/videogame.entity.ts
@@ -1,8 +1,25 @@
-import {Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn} from 'typeorm';
-import {Platform} from "./platform.entity";
-import {Developer} from "./developer.entity";
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    JoinTable,
+    ManyToMany, ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm';
+import { Platform } from "./platform.entity";
+import { Developer } from "./developer.entity";
 import { User } from '../user.entity';
 import { Genre } from './genre.entity';
+
+export enum Status {
+    DA_GIOCARE = "Da giocare",
+    IN_CORSO = "In corso",
+    FINITO = "Finito",
+    COMPLETATO = "Completato",
+    ABBANDONATO = "Abbandonato"
+}
 
 @Entity('vg_videogame')
 export class Videogame {
@@ -17,15 +34,6 @@ export class Videogame {
 
     @Column()
     year: number
-
-    // user library
-    @ManyToMany(() => User)
-    @JoinTable({
-        name: 'vg_user_videogame',
-        joinColumn: { name: 'user_id', referencedColumnName: 'id'},
-        inverseJoinColumn: {name: 'videogame_id', referencedColumnName: 'id'}
-    })
-    users: User[];
 
     @ManyToMany(() => Platform)
     @JoinTable({
@@ -58,8 +66,33 @@ export class Videogame {
         inverseJoinColumn: {name: 'remake', referencedColumnName: 'id'}
     })
     videogames: Videogame[];
+}
 
 
+@Entity('vg_user_videogame')
+export class VideogameUserVideogame {
+    @CreateDateColumn({type: 'timestamp'})
+    createdAt: Date
 
+    @UpdateDateColumn({type: 'timestamp'})
+    updatedAt: Date
 
+    @Column({default: 0})
+    hours: number;
+
+    @Column({default: false})
+    bought: boolean;
+
+    @Column({default: Status.DA_GIOCARE})
+    status: Status;
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: "user_id"})
+    @Column({primary: true})
+    user_id: number
+
+    @ManyToOne(() => Videogame)
+    @JoinColumn({name: "videogame_id"})
+    @Column({primary: true})
+    videogame_id: number
 }
