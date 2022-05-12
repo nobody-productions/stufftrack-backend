819aa6dfa9442f27f62c8c859c71f007c4544344
dag7dev <dag7@protonmail.com>
2022-05-12 11:55:03 +0200

    feat: rating seed

---
+++ src/seeder/videogame/rating.seed.ts
@@ -0,0 +1,46 @@
+import {createConnections, getManager} from "typeorm";
+import { Rating } from "../../entity/videogame/rating.entity";
+import {User} from "../../entity/user.entity";
+import {Videogame} from "../../entity/videogame/videogame.entity";
+
+createConnections().then(async () => {
+    const ratingRepository = getManager().getRepository(Rating);
+
+    //
+    let userTarget = await getManager().getRepository(User).findOne({where: {id: 1}})
+    let vgTarget = await getManager().getRepository(Videogame).findOne({where: {id: 1}})
+
+
+    await ratingRepository.save({
+        comment: "",
+        ranking: 9,
+        is_public_comment: true,
+        is_public_ranking: true,
+        user: userTarget,
+        videogame: vgTarget
+    })
+
+    //
+    vgTarget = await getManager().getRepository(Videogame).findOne({where: {id: 6}})
+
+    await ratingRepository.save({
+        comment: 'Terza generazione di pokémon: il gioco é il classico capitolo, ma qui hanno aggiunto delle cosine carine tipo la base segreta o il mago del quiz che propone enigmi. Non mi piacciono gli elementi grafici e in generale é un po troppo lento',
+        ranking: 7,
+        is_public_comment: true,
+        is_public_ranking: true,
+        user: userTarget,
+        videogame: vgTarget
+    })
+
+    //
+    vgTarget = await getManager().getRepository(Videogame).findOne({where: {id: 7}})
+
+    await ratingRepository.save({
+        comment: 'Mediocre, mi aspettavo di meglio ma ci ho comunque giocato molto.',
+        ranking: 7,
+        is_public_comment: true,
+        is_public_ranking: true,
+        user: userTarget,
+        videogame: vgTarget
+    })
+})
