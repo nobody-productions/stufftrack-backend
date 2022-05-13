7a4b8f5774d331604c7538cc84eb250b497719c8
dag7dev <dag7@protonmail.com>
2022-05-13 14:48:03 +0200

    enh: route -> /videogames/ fetches platform too

--- src/controller/videogames/videogame.controller.ts
+++ src/controller/videogames/videogame.controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import {getManager} from "typeorm";
 import { Videogame } from "../../entity/videogame/videogame.entity";
+import {Platform} from "../../entity/videogame/platform.entity";
 
 // get all videogames
 export const Videogames = async (req: Request, res: Response) => {
@@ -12,6 +13,21 @@ export const Videogames = async (req: Request, res: Response) => {
         take,
         skip: (page - 1) * take
     });
+
+    // mi prendo le piattaforme
+    for (const item of data) {
+        const platOnly = await getManager().getRepository(Platform)
+            .createQueryBuilder()
+            .select('vg_platform')
+            .from(Platform, "vg_platform")
+            .innerJoin('vg_videogame_platform', 'vg_videogame_platform', 'vg_platform.id = vg_videogame_platform.platform_id')
+            .innerJoin('vg_videogame', 'vg_videogame', 'vg_videogame.id = vg_videogame_platform.videogame_id')
+            .andWhere("vg_videogame.id = :id", {id: item['id']})
+
+        // unisco le due cose: piattaforme sul quale il gioco é uscito + videogioco stesso
+        item.platforms = await platOnly.getMany();
+    }
+
 
     res.send({
         data,
