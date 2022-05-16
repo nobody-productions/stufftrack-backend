e143a3a6f938881058debc4f04869c2b20f685e6
dag7dev <dag7@protonmail.com>
2022-05-16 14:23:29 +0200

    img: default images -> user seeder has been upd

--- src/seeder/user.seed.ts
+++ src/seeder/user.seed.ts
@@ -13,7 +13,7 @@ createConnections().then(async () => {
         email: "damiano@gmail.com",
         password: await bcryptjs.hash('BackendistaSenzaSosta11', 10),
         nickname: "dag7",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "Mi piacciono i videogiochi ma odio Toad di Mario Kart. Firma anche tu la petizione per eliminarlo dal gioco: change.org/eliminiamo-toad-da-mario-kart/",
         active: true,
         role: targetRole
@@ -23,7 +23,7 @@ createConnections().then(async () => {
         email: "lorenzo@gmail.com",
         password: await bcryptjs.hash('TelefoniChePassione!', 10),
         nickname: "Lollo",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "Mi piace testare i giochi su diversi dispositivi Android, i Mediatek fanno schifo!",
         active: true,
         role: targetRole
@@ -33,7 +33,7 @@ createConnections().then(async () => {
         email: "misterkrub@gmail.com",
         password: await bcryptjs.hash('INeedADollar$!', 10),
         nickname: "Krubby Krubbone",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "Ciao, mi piacciono i soldi.",
         active: true,
         role: targetRole
@@ -43,7 +43,7 @@ createConnections().then(async () => {
         email: "spongebob@gmail.com",
         password: await bcryptjs.hash('Gary4832930382749274', 10),
         nickname: "Spongy",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "Adoro dare da mangiare a Gary, la mia lumachina. I miei giochi preferiti sono i platform!",
         active: true,
         role: targetRole
@@ -53,7 +53,7 @@ createConnections().then(async () => {
         email: "patrickstella@gmail.com",
         password: await bcryptjs.hash('Blargh', 10),
         nickname: "Patrick Non Stella",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "WADJIOA3FN9W3FJ390FJ90FJWIEFSKJFS EFJSIEOJF8FJ 3WJF3I FWIOJFIWOEJF W39F93W 0F CIAO",
         active: true,
         role: targetRole
@@ -63,7 +63,7 @@ createConnections().then(async () => {
         email: "squidditentacolo@gmail.com",
         password: await bcryptjs.hash('Clarinetto63', 10),
         nickname: "Mastro Squiddi",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "Quando non suono il clarinetto, sono il campione mondiale di Osu. Gioco solo a giochi musicali.",
         active: true,
         role: targetRole
@@ -73,7 +73,7 @@ createConnections().then(async () => {
         email: "theshadow2030@gmail.com",
         password: await bcryptjs.hash('SplatoonIsMyLife', 10),
         nickname: "TheShadow2030",
-        propic: "/useruploads/default.png",
+        propic: "/profilepictures/default.png",
         bio: "Sono una persona temuta e rispettata su Splatoon, ma adoro i gdr o i giochi come God of War.",
         active: true,
         role: targetRole
