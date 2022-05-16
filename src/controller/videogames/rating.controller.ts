a0c84ee0c198b9af07a37df0fdf0b91db397c9f3
dag7dev <dag7@protonmail.com>
2022-05-16 22:45:10 +0200

    fix: create and get rating should have user and platf id

--- src/controller/videogames/rating.controller.ts
+++ src/controller/videogames/rating.controller.ts
@@ -2,30 +2,41 @@ import {Request, Response} from "express";
 import {createQueryBuilder, getManager, getRepository} from "typeorm";
 import {Rating} from "../../entity/videogame/rating.entity";
 import {Videogame} from "../../entity/videogame/videogame.entity";
+import {User} from "../../entity/user.entity";
 
 export const GetVideogameUserLibraryRating = async (req: Request, res: Response) => {
     const query = await
-        getRepository('vg_rating')
-            .createQueryBuilder("vg_rating")
+        getRepository(Rating)
+            .createQueryBuilder("")
             .where("videogame_id = :id", { id: req.params.id })
             .andWhere('user_id = :user_id', { user_id: req['user'].id })
             .getOne();
 
-    // const result = await query.execute();
-
-    res.status(200).send(query)
+    if(query !== null) {
+        res.send(query)
+    }
+    else
+        res.status(404).send('rating not found!')
 }
 
 export const CreateVideogameUserLibraryRating = async(req: Request, res: Response) => {
     const repository = getManager().getRepository(Rating);
 
     // forza lo user id dell'utente loggato cosi in caso di dati "strani" vengono fatti sull'utente corrente
-    req.body.user = req['user'].user
+    let userTarget = await getManager().getRepository(User).findOne({where: {id: req['user'].id}})
+    let vgTarget = await getManager().getRepository(Videogame).findOne({where: {id: parseInt(req.params.id)}})
+    req.body.user = userTarget
+    req.body.videogame = vgTarget
 
     const rating = await repository.save(req.body);
 
     // TODO: sistemare in caso di duplicati
-    res.status(201).send(rating);
+    if(rating !== null) {
+        delete rating.user
+        return res.status(201).send(rating)
+    }
+    else
+        return res.status(500).send('internal server error')
 }
 
 export const UpdateVideogameUserLibraryRating = async(req: Request, res: Response) => {
