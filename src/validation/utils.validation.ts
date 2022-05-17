3889a1666ac7cdcf67c72f2a7935c05e5214d0eb
dag7dev <dag7@protonmail.com>
2022-05-17 13:22:16 +0200

    docs: what does isInvalidPostgresNumber does

--- src/validation/utils.validation.ts
+++ src/validation/utils.validation.ts
@@ -1,4 +1,8 @@
 // common functions to check things
+
+// chk: is a number and not a string or smth else
+// chk: is an integer
+// chk: is between supported postgres range 1 and 2147483647
 export function isValidPostgresNumber(param: any): boolean {
     let retValue: boolean = false
     if ((!isNaN(Number(param)) && Number.isInteger(parseInt(param)) && (parseInt(param) >= 1 && parseInt(param) < 2147483647))) {
