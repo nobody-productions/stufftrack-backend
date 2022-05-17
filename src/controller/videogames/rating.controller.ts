a1b721bfc6c6469fecdb5dea9171e2a2d41a74bf
dag7dev <dag7@protonmail.com>
2022-05-17 16:10:07 +0200

    enh: rating now includes dupe check

--- src/controller/videogames/rating.controller.ts
+++ src/controller/videogames/rating.controller.ts
@@ -8,29 +8,37 @@ export const GetVideogameUserLibraryRating = async (req: Request, res: Response)
     const query = await
         getRepository(Rating)
             .createQueryBuilder("")
-            .where("videogame_id = :id", { id: req.params.id })
-            .andWhere('user_id = :user_id', { user_id: req['user'].id })
+            .where("Rating.videogame = :id", { id: parseInt(req.params.id) })
+            .andWhere('Rating.user = :user_id', { user_id: req['user'].id })
             .getOne();
 
     if(query !== null) {
         res.send(query)
     }
     else
-        res.status(404).send('rating not found!')
+        res.status(404).send({message: 'rating not found!'})
 }
 
 export const CreateVideogameUserLibraryRating = async(req: Request, res: Response) => {
     const repository = getManager().getRepository(Rating);
 
-    // forza lo user id dell'utente loggato cosi in caso di dati "strani" vengono fatti sull'utente corrente
     let userTarget = await getManager().getRepository(User).findOne({where: {id: req['user'].id}})
     let vgTarget = await getManager().getRepository(Videogame).findOne({where: {id: parseInt(req.params.id)}})
-    req.body.user = userTarget
-    req.body.videogame = vgTarget
+    req.body.user = userTarget.id
+    req.body.videogame = vgTarget.id
+
+    // chk: rating already exists
+    const chkRating = await getRepository(Rating).createQueryBuilder()
+        .andWhere(`Rating.videogame = :videogame`, { videogame: vgTarget.id})
+        .andWhere(`Rating.user = :user`, { user: userTarget.id})
+        .getOne();
+
+    if(chkRating !== null) {
+        return res.status(409).send({message: "rating already exists!"})
+    }
 
     const rating = await repository.save(req.body);
 
-    // TODO: sistemare in caso di duplicati
     if(rating !== null) {
         delete rating.user
         return res.status(201).send(rating)
@@ -59,7 +67,7 @@ export const DeleteVideogameUserLibraryRating = async (req: Request, res: Respon
     const vg = await getManager().getRepository(Videogame).findOne({where: {id: parseInt(req.params.id)}})
     await getRepository('vg_rating').createQueryBuilder()
         .delete()
-        .andWhere(`videogame_id = :videogame`, { videogame: vg.id})
+        .andWhere(`videogame = :videogame`, { videogame: vg.id})
         .andWhere(`user = :user`, { user: req['user'].id})
         .execute();
     return res.status(204).send(null)
