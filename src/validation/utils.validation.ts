8f881e191e12d6e8146d26117413367fe4f0486a
dag7dev <dag7@protonmail.com>
2022-05-17 14:59:48 +0200

    feat: check if a number is a valid number, and it's in a valid range

--- src/validation/utils.validation.ts
+++ src/validation/utils.validation.ts
@@ -3,9 +3,10 @@
 // chk: is a number and not a string or smth else
 // chk: is an integer
 // chk: is between supported postgres range 1 and 2147483647
+
 export function isValidPostgresNumber(param: any): boolean {
     let retValue: boolean = false
-    if ((!isNaN(Number(param)) && Number.isInteger(parseInt(param)) && (parseInt(param) >= 1 && parseInt(param) < 2147483647))) {
+    if ((!isNaN(param) && Number.isInteger(param) && (param >= 1 && param < 2147483647))) {
         retValue = true
     }
     return retValue
