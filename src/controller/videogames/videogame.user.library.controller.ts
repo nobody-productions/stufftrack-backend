5188ab14bb23b05d5ce17f45baa31c66fd914608
dag7dev <dag7@protonmail.com>
2022-05-17 18:40:29 +0200

    refactor: userlibrary.validation > videogame.user.library.validation

--- src/controller/videogames/videogame.user.library.controller.ts
+++ src/controller/videogames/videogame.user.library.controller.ts
@@ -5,7 +5,7 @@ import { Videogame } from "../../entity/videogame/videogame.entity";
 import {Platform} from "../../entity/videogame/platform.entity";
 import {
     VideogameUserLibraryValidation
-} from "../../validation/userlibrary.validation";
+} from "../../validation/videogame.user.library.validation";
 import {isValidPostgresNumber} from "../../validation/utils.validation";
 
 // get all videogames in user library
